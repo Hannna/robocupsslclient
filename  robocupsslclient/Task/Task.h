9d21c865da9ed99017256a874f5a5da0d9a5e56d
mgabka <mgabka@48f5bffa-9504-1908-88fd-a439b3b414de>
2010-11-09 09:59:40 +0000

    zmiana w task

---  robocupsslclient/Task/Task.h
+++  robocupsslclient/Task/Task.h
@@ -10,19 +10,22 @@
 #include "../additional.h"
 #include "../Robot/Robot.h"
 #include "../VideoServer/Videoserver.h"
+#include <boost/thread/mutex.hpp>
 
 //class Videoserver;
 
 class Task {
 public:
 	Task(Robot *robot);
-	virtual bool execute()=0;
+	virtual bool execute();
 	virtual void stop();
 	virtual ~Task();
 protected:
+	virtual bool run()=0;
 	const Videoserver & video;
 	bool stopTask;
 	Robot * robot;
+	boost::mutex mutex_;
 };
 
 #endif /* TASK_H_ */
